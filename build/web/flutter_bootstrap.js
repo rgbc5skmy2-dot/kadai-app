8f51e5324b68fd914dc5a8f52ccbb608a58f6c62
RGBC5SKY2-DOT <rgbc5skmy2@privaterelay.appleid.com>
2026-05-20 20:55:37 +0900

    アプリ名をSubmitrに変更

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "953260717" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2962090420" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- lib/screens/task_list_screen.dart
+++ lib/screens/task_list_screen.dart
@@ -134,7 +134,7 @@ class _TaskListScreenState extends State<TaskListScreen>
         backgroundColor: const Color(0xFFF5F6FA),
         elevation: 0,
         title: const Text(
-          'Submitr',
+          '課題管理',
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w700,
