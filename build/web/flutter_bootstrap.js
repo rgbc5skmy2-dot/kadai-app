3d7ff1b9f2dddcd3a9551c57a103e2a64b7fa409
RGBC5SKY2-DOT <rgbc5skmy2@privaterelay.appleid.com>
2026-05-25 22:15:04 +0900

    アイコン余白修正

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1697318784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3754029941" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
